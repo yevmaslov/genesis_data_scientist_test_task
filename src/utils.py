52a0b9caf35f94ccd2892a392d8e87d0cd8acbd8
EvgeniiMaslov <sqrt.evmaslov@gmail.com>
2021-09-07 14:17:19 +0300

    basic eda

---
+++ src/utils.py
@@ -0,0 +1,32 @@
+import requests
+
+
+def download_dataset(destination):
+    url = "https://docs.google.com/uc?export=download"
+    file_id = '15hj_kpNICz-Czl7hpDjOjxKlU6bElCRr'
+
+    session = requests.Session()
+
+    response = session.get(url, params={'id': file_id}, stream=True)
+    token = get_confirm_token(response)
+
+    if token:
+        params = {'id': file_id, 'confirm': token}
+        response = session.get(url, params=params, stream=True)
+
+    save_response_content(response, destination)
+
+
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith('download_warning'):
+            return value
+    return None
+
+
+def save_response_content(response, destination):
+    chunk_size = 32768
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(chunk_size):
+            if chunk:
+                f.write(chunk)
